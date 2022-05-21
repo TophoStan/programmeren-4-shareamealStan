e8dd6a93e04544bbde2cca1a9408175e1d7771c4
TophoStan <saj.tophoven@student.avans.nl>
2022-05-21 12:22:48 +0200

    all tests are finished

--- src/routes/Meal.js
+++ src/routes/Meal.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mealController = require("../controllers/meal.controller");
 const authController = require("../controllers/auth.controller");
+const particpationController = require("../controllers/participation.controller");
 
 router.get("/", mealController.getAllMeals);
 router.get("/:id", mealController.validateId, mealController.getMealById);
@@ -18,5 +19,10 @@ router.put(
   mealController.updateMeal
 );
 router.delete("/:id", authController.validateToken, mealController.deleteMeal);
+router.get(
+  "/:id/participate",
+  authController.validateToken,
+  particpationController.participateInAMeal
+);
 
 module.exports = router;
